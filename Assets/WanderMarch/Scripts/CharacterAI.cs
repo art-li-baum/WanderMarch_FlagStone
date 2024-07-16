e72ba3ad7b0caf8d05329491ff1a95c4f95677af
art-li-baum <art.li.baum@gmail.com>
2024-07-16 11:38:40 -0700

    initial

---
+++ Assets/WanderMarch/Scripts/CharacterAI.cs
@@ -0,0 +1,92 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace WanderMarch.Scripts
+{
+    [RequireComponent(typeof(CharacterMotor))]
+    public class CharacterAI : MonoBehaviour
+    {
+        private CharacterMotor _motor;
+        
+        [SerializeField] private float lapTimer = 1f;
+        private int _lap = 0;
+        private float _l = 0;
+        
+        private enum State
+        {
+            Laps,
+            SideToSide,
+        }
+
+       [SerializeField] private State _currentState = State.Laps;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            _motor = GetComponent<CharacterMotor>();
+        }
+
+        // Update is called once per frame
+        public void Update()
+        {
+            switch (_currentState)
+            {
+                case State.Laps:
+                {
+                    _l += Time.deltaTime;
+                    if (_l < lapTimer) break;
+                    switch(_lap) 
+                    {
+                        case 0:
+                            _motor.SetInput(Vector2.down);
+                            _lap = 1;
+                            _l = 0;
+                            break;
+                        case 1:
+                            _motor.SetInput(Vector2.left);
+                            _lap = 2;
+                            _l = 0;
+                            break;
+                        case 2:
+                            _motor.SetInput(Vector2.up);
+                            _lap = 3;
+                            _l = 0;
+                            break;
+                        case 3:
+                            _motor.SetInput(Vector2.right);
+                            _lap = 0;
+                            _l = 0;
+                            break;
+                    }
+
+                    break;
+                }
+                case State.SideToSide:
+                {
+                    _l += Time.deltaTime;
+                    if (_l < lapTimer) break;
+                    _l = 0;
+                    if (_lap == 0)
+                    {
+                        _lap = 1;
+                        _motor.SetInput(Vector2.right);
+                    }
+                    else
+                    {
+                        _lap = 0;
+                        _motor.SetInput(Vector2.left);
+                    }
+                }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void ApplyInput()
+        {
+            
+        }
+    }
+}
---
+++ Assets/WanderMarch/Scripts/CharacterMotor.cs
@@ -0,0 +1,81 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+using WanderMarch.Scripts.Math;
+
+namespace WanderMarch.Scripts
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class CharacterMotor : MonoBehaviour
+    {
+        private Rigidbody2D _body;
+
+        private Vector2 _inputDirection;
+
+        [Header("Parameters")] [SerializeField]
+        private float maxSpeed = 10f;
+
+        [SerializeField, Range(1f, 25f), Tooltip("How 'steep' the acceleration curve is")]
+        private float maxAccelerationFactor = 16f;
+
+        [SerializeField, Range(1f, 25f), Tooltip("The 'slide' factor when stopping")]
+        private float maxDecelerationFactor = 20f;
+
+        [SerializeField, Range(1f, 25f), Tooltip("How fast the motor accelerates/decelerates")]
+        private float jerk;
+
+        [SerializeField] private AnimationCurve TurnAroundFactor;
+
+        [Header("Current Stats")] [SerializeField]
+        private bool visualizeVelocityVectors = false;
+
+        [SerializeField, ShowOnly] private Vector2 velocity;
+
+        [SerializeField, ShowOnly] private float accelerationFactor;
+        [SerializeField, ShowOnly] private Vector2 goalVelocity;
+
+        public void SetInput(Vector2 input)
+        {
+            _inputDirection = input;
+
+            //TODO Messaging?
+        }
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _body = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            goalVelocity = _inputDirection * maxSpeed;
+
+            //Can give more power for sharp turns to create snappier controls
+            var turnBoost = TurnAroundFactor.Evaluate(Vector2.Dot(goalVelocity.normalized, velocity.normalized));
+
+            accelerationFactor = MathUtil.FloatDecay(accelerationFactor, //Decelerate if no input
+                Mathf.Abs(goalVelocity.magnitude) > 0 ? maxAccelerationFactor * turnBoost : maxDecelerationFactor, jerk,
+                Time.fixedDeltaTime);
+
+
+            //Update the velocity by applying acceleration
+            velocity = MathUtil.Vector2Decay(velocity, goalVelocity, accelerationFactor, Time.fixedDeltaTime);
+
+
+            //Update position by applying velocity
+            _body.position += (velocity * Time.fixedDeltaTime);
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!visualizeVelocityVectors) return;
+            Vector3 vel = velocity;
+            Vector3 gvel = goalVelocity;
+            var position = transform.position;
+
+            Debug.DrawLine(position, position + vel, Color.blue);
+            Debug.DrawLine(position, position + gvel, Color.green);
+        }
+    }
+}
