5a4b900f09bf3750ece676b27faa87dda3cbcc10
art-li-baum <art.li.baum@gmail.com>
2024-12-30 10:47:03 -0800

    Initial commit with some spline maths

---
+++ Assets/WanderMarch/Scripts/Spline/SplinePath.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+namespace WanderMarch.Scripts.Spline
+{
+    public class SplinePath : MonoBehaviour
+    {
+        [SerializeField] private BezierSegment spline = new();
+
+        [SerializeField, Range(0.0f, 1.0f), Tooltip("Used to visualize point along path")]
+        private float tTest;
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            spline.DrawGizmo();
+            OrientedPoint testPoint = spline.GetBezierPoint(tTest);
+
+            Gizmos.color = Color.red;
+            
+           Gizmos.DrawSphere(testPoint.position, 0.1f);
+        }
+    }
+#endif
+}
